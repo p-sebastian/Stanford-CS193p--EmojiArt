af76bbfa3847d44f830f83fa591e42b83d1f6eb4
Sebastian Penafiel <penafiel.sebastian@gmail.com>
2020-07-05 13:32:39 -0500

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Sebastian Penafiel on 7/5/20.
+//  Copyright © 2020 Sebastian Penafiel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
